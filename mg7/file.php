7408973093c7dacddea707a7066b79166addf5ba
Retha <92181383+MargarethaViolinaPutri@users.noreply.github.com>
2022-04-10 21:35:20 +0700

    Add files via upload

---
+++ mg7/file.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html> 
+    <head>
+    </head>
+    <body>
+        <?php 
+        $myFile = "testFile.txt";
+        $fh = fopen($myFile, 'r');
+        $theData = fread($fh, filesize($myFile));
+        fclose($fh);
+        echo $theData;
+        ?>
+    </body>
+</html>
---
+++ mg7/file_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html> 
+    <head>
+    </head>
+    <body>
+        <?php 
+        $myFile = "testFile.txt";
+        $fh = fopen($myFile, 'r');
+        $theData = fgets($fh);
+        fclose($fh);
+        echo $theData;
+        ?>
+    </body>
+</html>
